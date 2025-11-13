e8d32d5587e53c19292297bbbc5268196bfab839
WhoYouAndM3 <83360444+l3ger0j@users.noreply.github.com>
2025-11-14 02:19:48 +0300

    Remove unused `Bundle` import

--- app/src/main/java/com/pixnpunk/qsplugin/RealRootComponent.kt
+++ app/src/main/java/com/pixnpunk/qsplugin/RealRootComponent.kt
@@ -4,7 +4,6 @@ import android.content.ActivityNotFoundException
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
-import android.os.Bundle
 import android.util.Log
 import android.webkit.WebResourceRequest
 import android.webkit.WebResourceResponse
